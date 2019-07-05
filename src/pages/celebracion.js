5d95890ce40fc5a672cb152161c416500317efb1
chema <didelco@gmail.com>
2019-07-05 11:32:17 +0200

    tanya

--- src/pages/celebracion.js
+++ src/pages/celebracion.js
@@ -12,7 +12,7 @@ export default () => (
               La boda de Emilio y Ana estará llena de sorpresas, por lo que no vamos a desvelar mucho sobre la celebración. Para que no os
               despisteis, sólo debéis saber que tras la ceremonia se celebrará un coctel y después se pasará al salón donde empezará la
               comida. En el transcurso de esta, hemos organizado algunos juegos que explicamos en el apartado Juegos de esta página. Podéis
-              consultar cualquier duda a las madrinas, Tania e Isabel. Todo lo demás... misterio y diversión!! ☺️
+              consultar cualquier duda a las madrinas, Tanya e Isabel. Todo lo demás... misterio y diversión!! ☺️
             </p>
             <p>En todo el recinto del Cigarral hay WIFI abierta, no necesita contraseña.</p>
           </div>
@@ -23,7 +23,7 @@ export default () => (
               Die Hochzeit von Emilio und Ana wird voller Überraschungen sein, daher werden wir nicht viel über die Feier verraten. Damit
               Sie sich nicht verlaufen, sollten Sie nur wissen, dass nach der Zeremonie ein Aperitif abgehalten wird und Sie dann in den
               Raum gehen, in dem das Essen beginnen wird. Im Zuge dessen haben wir einige Spiele organisiert, die wir im Abschnitt Spiele
-              auf dieser Seite erklären. Sie können Fragen an die Patinnen Tania und Isabel wenden. Alles andere ... Rätsel und Spaß !! ☺️
+              auf dieser Seite erklären. Sie können Fragen an die Patinnen Tanya und Isabel wenden. Alles andere ... Rätsel und Spaß !! ☺️
             </p>
             <p>Im gesamten Cigarral ist WLAN offen, Sie benötigen kein Passwort.</p>
           </div>
